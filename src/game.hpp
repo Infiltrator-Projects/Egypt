6085d3d81bea30d7d96836392fc351b2e9c95195
Shannon Smith <infiltratr@yandex.com>
2026-09-12 20:50:23 +1000

    Wire graphical HUD surfaces into game state

--- src/game.hpp
+++ src/game.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include "ui_core.hpp"
+#include "common/raw_image.hpp"
 
 namespace egypt {
 
 class Game {
 #include "game_public.inl"
 #include "game_state.inl"
+#include "game_graphics_state.inl"
 #include "game_buildings.inl"
 #include "game_terrain.inl"
 #include "game_agents.inl"
