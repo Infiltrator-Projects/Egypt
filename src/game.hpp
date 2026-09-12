25094060bf50a6036482adbd62c8275555eb3711
Shannon Smith <infiltratr@yandex.com>
2026-09-12 15:33:42 +1000

    Assemble Egypt city game presentation modules

---
+++ src/game.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "ui_core.hpp"
+
+namespace egypt {
+
+class Game {
+#include "game_public.inl"
+#include "game_state.inl"
+#include "game_buildings.inl"
+#include "game_agents.inl"
+#include "game_world_ui.inl"
+#include "game_inspector.inl"
+};
+
+} // namespace egypt
